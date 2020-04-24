d3ec0f4ec0c435d41af7d019a68fa553521ea6f0
Divyansh Srivastava <srivastavadivyansh121@gmail.com>
2020-04-25 02:00:52 +0530

    dashboard

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Dashboard
 This Project contains a firebase login and button which fetches the electronic device details from API and display in dashboard
+# This is an DashBoard All device details Project
+## Tools used to build this project:
+>HTML
+>CSS
+>JAVASCRIPT
+>Firebase
+![landing page1](/images/landing1.png)
+![landing page2](/images/landing1.png)
+* In this Project the user can signUp and login into the dashboard
+* The user can fetch the details about the devices via fetch button.
---
+++ axious.js
@@ -0,0 +1,87 @@
+
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAb8eseTl2fTPNaGdkX9dtnqX9csjVRypw",
+    authDomain: "form-c8d1c.firebaseapp.com",
+    databaseURL: "https://form-c8d1c.firebaseio.com",
+    projectId: "form-c8d1c",
+    storageBucket: "form-c8d1c.appspot.com",
+    messagingSenderId: "916699410001",
+    appId: "1:916699410001:web:96afae95091ded5a5d5270"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+//function for new register
+function signUp() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password")
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    alert("Registered Successfully!!Now you may login");
+
+}
+//function for login
+function signIn() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password")
+    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
+    alert("signed in")
+    //takes user to different page
+    window.location.href = "file:///home/divyansh/coding/projects/Dashboard/dashboard.html";
+
+}
+//function for signOut
+function signOut() {
+    auth.signOut();
+    alert("signed out")
+    window.location.href = "file:///home/divyansh/coding/projects/Dashboard/index.html";
+
+}
+auth.onAuthStateChanged(function (user) {
+    if (user) {
+        var email = user.email;
+        alert("Active User " + email)
+    }
+    else {
+        alert("no active user")
+    }
+
+});
+//function for fetching all devices information & display in dashboard
+fetch = (num) => {
+    for (num = 1; num <= 4; num++) {
+        axios.get(`https://test.bettad.xyz/device/${num}`)
+            .then((response) => {
+                console.log(response.data.response);
+
+                var body = document.getElementById('dashbody')
+                var table = document.getElementById("tbl")
+                var row = document.createElement('tr')
+                var td1 = document.createElement('td')
+                var td2 = document.createElement('td')
+                var td3 = document.createElement('td')
+                var td4 = document.createElement('td')
+                td1.textContent = response.data.response.device_id
+                td2.textContent = response.data.response.device_name
+                td3.textContent = response.data.response.device_type
+                td4.textContent = response.data.response.device_power
+                row.appendChild(td1)
+                row.appendChild(td2)
+                row.appendChild(td3)
+                row.appendChild(td4)
+                table.appendChild(row)
+                body.appendChild(table)
+
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+}
+
+
+
+
+
